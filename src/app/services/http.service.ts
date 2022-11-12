8b4ffc956309f1c6c8e4159f22c5803279c310df
lukka8_8 <45329766+Lukka8@users.noreply.github.com>
2022-11-13 00:13:46 +0400

    Update http.service.ts

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -10,6 +10,16 @@ export class HttpService {
   domain:string = "http://localhost:8000/"
 
   constructor(private http:HttpClient) {}
+  getUserActive() {
+    return this.http.post(this.domain + "API/UserLoggedIn", null,{ })
+  }
 
+  loginUser(data: any) {
+    return this.http.post(this.domain + "API/LoginUser", null,{ })
+  }
+
+  registerUser(data: any) {
+    return this.http.post(this.domain + "API/RegisterUser", null, { })
+  }
 }
 
